21c64d150923717735bd5c0b4eae645a8cdaec10
Michael van Vierssen <mvierssen@gmail.com>
2025-10-12 19:14:31 +0200

    Added smells

--- packages/katas/smelly-mars-rover/src/Rover.ts
+++ packages/katas/smelly-mars-rover/src/Rover.ts
@@ -1,8 +1,11 @@
 import { RoverState } from "./RoverState.js";
 
 export class Rover {
+  // Code Smell: Primitive Obsession "p"
   constructor(p = "") {
+    // Code Smell: Magic String (" " delimiter)
     const s = p.split(" ");
+    // Code Smell: Data Clumps (s[0], s[1], s[2])
     if (s.length >= 3) {
       if (s[0] === undefined || s[1] === undefined || s[2] === undefined) {
         throw new Error("Invalid position string");
@@ -10,15 +13,19 @@ export class Rover {
       if (typeof s[2][0] !== "string") {
         throw new TypeError("Invalid direction");
       }
+      // Code Smell: Poor Naming "xx", Primitive Obsession, Feature Envy
       this.rs.xx = Number.parseInt(s[0], 10);
+      // Code Smell: Poor Naming "yy", Primitive Obsession, Feature Envy
       this.rs.yy = Number.parseInt(s[1], 10);
       this.rs.dd = s[2][0];
     }
   }
 
+  // Code Smell: Long Function, Primitive Obsession "cms", Poor Naming "cms"
   public go(cms: string): void {
     for (const c of cms) {
       switch (c) {
+        // Code Smell: Magic Strings ("L", "R", "M", "E", "N", "W", "S"), Feature Envy
         case "L": {
           switch (this.rs.dd) {
             case "E": {
@@ -94,6 +101,7 @@ export class Rover {
     }
   }
 
+  // Code Smell: Primitive Obsession
   public G(z: string): void {
     if (typeof z[0] !== "string") {
       throw new TypeError("Invalid direction");
@@ -101,10 +109,12 @@ export class Rover {
     this.go(z[0]);
   }
 
+  // Code Smell: Poor Naming "XYD", Primitive Obsession, Feature Envy
   public get XYD(): string {
     return `${String(this.rs.xx)} ${String(this.rs.yy)} ${this.rs.dd}`;
   }
 
+  // Code Smell: Primitive Obsession
   public pos(): string {
     return this.XYD;
   }
