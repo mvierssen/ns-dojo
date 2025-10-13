9654522666b9504c6a7fe0c3a3ec9d7bcbec3407
Michael van Vierssen <mvierssen@gmail.com>
2025-10-13 18:08:18 +0200

    refactor: use constants for directions and command

--- packages/katas/smelly-mars-rover/src/constants.ts
+++ packages/katas/smelly-mars-rover/src/constants.ts
@@ -1,25 +1,38 @@
-import type { Direction, Vector2D } from "./types.js";
+import type { Vector2D } from "./types.js";
+
+export enum Direction {
+  North = "N",
+  East = "E",
+  South = "S",
+  West = "W",
+}
+
+export enum Command {
+  Left = "L",
+  Right = "R",
+  Move = "M",
+}
 
 export const INSTRUCTION_STRING_REGEX = /^[LRM]*$/;
 export const POSITION_DIRECTION_STRING_REGEX = /^\d+ \d+ [NESW]$/;
 
 export const TURN_LEFT_TRANSITION_MAP: Record<Direction, Direction> = {
-  N: "W",
-  W: "S",
-  S: "E",
-  E: "N",
+  [Direction.North]: Direction.West,
+  [Direction.West]: Direction.South,
+  [Direction.South]: Direction.East,
+  [Direction.East]: Direction.North,
 };
 
 export const TURN_RIGHT_TRANSITION_MAP: Record<Direction, Direction> = {
-  N: "E",
-  E: "S",
-  S: "W",
-  W: "N",
+  [Direction.North]: Direction.East,
+  [Direction.East]: Direction.South,
+  [Direction.South]: Direction.West,
+  [Direction.West]: Direction.North,
 };
 
 export const MOVE_VECTOR_MAP: Record<Direction, Vector2D> = {
-  N: { x: 0, y: 1 },
-  E: { x: 1, y: 0 },
-  S: { x: 0, y: -1 },
-  W: { x: -1, y: 0 },
+  [Direction.North]: { x: 0, y: 1 },
+  [Direction.East]: { x: 1, y: 0 },
+  [Direction.South]: { x: 0, y: -1 },
+  [Direction.West]: { x: -1, y: 0 },
 };
