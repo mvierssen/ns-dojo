71725e9a97ccd45326d76d17138bb99403291707
Michael van Vierssen <michael.vanvierssen@ns.nl>
2025-10-14 11:37:42 +0200

    refactor: created safe method using parallel change and mikado

--- packages/shared/shared-core/src/types.ts
+++ packages/shared/shared-core/src/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const resultSchema = <T, E = string>(
   valueSchema: z.ZodType<T>,
-  errorSchema: z.ZodType<E>,
+  errorSchema: z.ZodType<E>
 ) =>
   z.union([
     z
@@ -25,14 +25,29 @@ export type Result<T, E = string> = z.infer<
 export type SuccessResult<T> = Extract<Result<T>, { success: true }>;
 export type FailureResult<E> = Extract<Result<never, E>, { success: false }>;
 
-export const createSuccessResult = <T>(value: T): SuccessResult<T> =>
+export const resultCreateSuccess = <T>(value: T): SuccessResult<T> =>
   ({
     success: true,
     value,
   }) as SuccessResult<T>;
 
-export const createFailureResult = <E>(error: E): FailureResult<E> =>
+export const resultCreateFailure = <E>(error: E): FailureResult<E> =>
   ({
     success: false,
     error,
   }) as FailureResult<E>;
+
+export const resultIsSuccess = <T, E>(r: Result<T, E>): r is SuccessResult<T> =>
+  r.success;
+export const resultIsFailure = <T, E>(r: Result<T, E>): r is FailureResult<E> =>
+  !r.success;
+
+export const resultMap = <T, U, E>(
+  r: Result<T, E>,
+  fn: (t: T) => U
+): Result<U, E> => (resultIsSuccess(r) ? resultCreateSuccess(fn(r.value)) : r);
+
+export const resultFlatMap = <T, U, E>(
+  r: Result<T, E>,
+  fn: (t: T) => Result<U, E>
+): Result<U, E> => (resultIsSuccess(r) ? fn(r.value) : r);
--- packages/shared/shared-core/test/types.test.ts
+++ packages/shared/shared-core/test/types.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, it } from "vitest";
-import { createFailureResult, createSuccessResult } from "../src/types.js";
+import { resultCreateFailure, resultCreateSuccess } from "../src/types.js";
 
 describe("result", () => {
   it("should create a success result with a value", () => {
     const value = { data: "test" };
-    const result = createSuccessResult(value);
+    const result = resultCreateSuccess(value);
     expect(result).toEqual({ success: true, value });
   });
 
   it("should create a failure result with an error", () => {
     const error = "Error. Transaction rejected. balance too low";
-    const result = createFailureResult(error);
+    const result = resultCreateFailure(error);
     expect(result).toEqual({ success: false, error });
   });
 });
