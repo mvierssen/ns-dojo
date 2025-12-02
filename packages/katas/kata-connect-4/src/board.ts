73ba842ff4f90ae3e9b052726803a2d9fa3e208b
Michael van Vierssen <mvierssen@gmail.com>
2025-12-02 15:49:48 +0100

    test: board has 7 columns

--- packages/katas/kata-connect-4/src/board.ts
+++ packages/katas/kata-connect-4/src/board.ts
@@ -1,10 +1,14 @@
-import { BOARD_ROWS } from "./constants.js";
+import { BOARD_COLUMNS, BOARD_ROWS } from "./constants.js";
 import type { Board } from "./types.js";
 
 export function createBoard(): Board {
   const cells: string[][] = [];
   for (let i = 0; i < BOARD_ROWS; i++) {
-    cells.push([]);
+    const row: string[] = [];
+    for (let j = 0; j < BOARD_COLUMNS; j++) {
+      row.push("");
+    }
+    cells.push(row);
   }
   return { cells };
 }
