88b1e5f6c1b0d416cd8b8e556bd1c42280d484c3
Michael van Vierssen <michael.vanvierssen@ns.nl>
2026-01-13 17:22:27 +0100

    test: column input schema accepts valid column 7

--- packages/katas/kata-connect-4/src/schemas.test.ts
+++ packages/katas/kata-connect-4/src/schemas.test.ts
@@ -6,4 +6,9 @@ describe("ColumnInputSchemaShould", () => {
     const result = ColumnInputSchema.safeParse(1);
     expect(result.success).toBe(true);
   });
+
+  test("accept valid column 7", () => {
+    const result = ColumnInputSchema.safeParse(7);
+    expect(result.success).toBe(true);
+  });
 });
