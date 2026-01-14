41e007a2e7f8896eacb2e663dcb37a8436f4de65
Michael van Vierssen <michael.vanvierssen@ns.nl>
2026-01-14 22:49:59 +0100

    test: add IGame interface

--- packages/katas/kata-connect-4/src/types.ts
+++ packages/katas/kata-connect-4/src/types.ts
@@ -1,7 +1,19 @@
+import type {Result} from "@ns-dojo/shared-core";
 import type {z} from "zod";
+import type {CellState} from "./constants.js";
+import type {GameInstructions} from "./instructions.js";
 import type {BoardSchema, PositionSchema} from "./schemas.js";
 
 export type Board = z.infer<typeof BoardSchema>;
 export type Position = z.infer<typeof PositionSchema>;
 
 export type Column = number & {readonly __brand: "Column"};
+
+export interface IGame {
+  start(): void;
+  getBoard(): Board;
+  getInstructions(): GameInstructions;
+  displayBoard(): string;
+  validateColumnInput(input: string): Result<Column>;
+  dropCoin(column: Column, player: CellState): Result<Position>;
+}
