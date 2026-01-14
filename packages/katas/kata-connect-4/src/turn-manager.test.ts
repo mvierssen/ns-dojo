a617a4fd0178d1d2f8b70f1a57f552d337185490
Michael van Vierssen <mvierssen@gmail.com>
2026-01-14 19:52:59 +0100

    test: add turn advancement to TurnManager

--- packages/katas/kata-connect-4/src/turn-manager.test.ts
+++ packages/katas/kata-connect-4/src/turn-manager.test.ts
@@ -1,10 +1,25 @@
 import {describe, expect, test} from "vitest";
-import {createTurnManager} from "./turn-manager.js";
+import {createTurnManager, advanceTurn} from "./turn-manager.js";
 import {CellState} from "./constants.js";
 
 describe("TurnManagerShould", () => {
   test("start with Player1 as current player", () => {
     const turnManager = createTurnManager();
     expect(turnManager.currentPlayer).toBe(CellState.Player1);
+  });
+
+  test("switch to Player2 after Player1 move", () => {
+    const turnManager = createTurnManager();
+    const updated = advanceTurn(turnManager);
+
+    expect(updated.currentPlayer).toBe(CellState.Player2);
+  });
+
+  test("switch to Player1 after Player2 move", () => {
+    const turnManager = createTurnManager();
+    const afterFirst = advanceTurn(turnManager);
+    const afterSecond = advanceTurn(afterFirst);
+
+    expect(afterSecond.currentPlayer).toBe(CellState.Player1);
   });
 });
