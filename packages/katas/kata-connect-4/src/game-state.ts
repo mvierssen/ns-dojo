44a37c05e72c2c2c174820696190df930b47f0fc
Michael van Vierssen <mvierssen@gmail.com>
2026-01-14 20:29:59 +0100

    test: add processMove function

--- packages/katas/kata-connect-4/src/game-state.ts
+++ packages/katas/kata-connect-4/src/game-state.ts
@@ -1,5 +1,13 @@
+import {
+  resultCreateFailure,
+  resultCreateSuccess,
+  resultIsSuccess,
+  type Result,
+} from "@ns-dojo/shared-core";
 import {createBoard} from "./board-core.js";
-import type {Board} from "./types.js";
+import {dropCoin} from "./board-logic.js";
+import type {CellState} from "./constants.js";
+import type {Board, Position} from "./types.js";
 
 export interface GameState {
   board: Board;
@@ -9,4 +17,23 @@ export function initializeGameState(): GameState {
   return {
     board: createBoard(),
   };
+}
+
+export function processMove(
+  gameState: GameState,
+  column: number,
+  player: CellState
+): Result<{gameState: GameState; position: Position}> {
+  const dropResult = dropCoin(gameState.board, column, player);
+
+  if (!resultIsSuccess(dropResult)) {
+    return resultCreateFailure(dropResult.error);
+  }
+
+  return resultCreateSuccess({
+    gameState: {
+      board: dropResult.value.board,
+    },
+    position: dropResult.value.position,
+  });
 }
