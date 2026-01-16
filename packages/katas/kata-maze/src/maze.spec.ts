1249372f2369d5e3c2da32def62733932cad0156
Michael van Vierssen <michael.vanvierssen@ns.nl>
2026-01-16 11:49:22 +0100

    chore: importes js maze to ts

---
+++ packages/katas/kata-maze/src/maze.spec.ts
@@ -0,0 +1,45 @@
+import { Maze } from './maze.js';
+import { describe, expect, test } from 'vitest';
+
+// mazes are provided.  Write your own tests!
+
+const SIMPLE_MAZE = [
+  ['S', '0', '1'],
+  ['1', '0', '1'],
+  ['1', '0', 'E'],
+];
+
+// const MODERATE_MAZE = [
+//   ['S', '0', '0', '1', 'E'],
+//   ['1', '0', '1', '1', '0'],
+//   ['1', '0', '0', '1', '0'],
+//   ['0', '0', '1', '0', '0'],
+//   ['1', '0', '0', '0', '0'],
+// ];
+
+// const LARGE_MAZE = [
+//   ['S','1','1','0','1','1','0','0','0','1','1','1','1'],
+//   ['1','0','1','1','1','0','1','1','1','1','0','0','1'],
+//   ['0','0','0','0','1','0','1','0','1','0','1','0','0'],
+//   ['1','1','1','0','1','1','1','0','1','0','1','1','1'],
+//   ['1','0','1','0','0','0','0','1','1','1','0','0','1'],
+//   ['1','0','1','1','1','1','1','1','0','1','1','1','1'],
+//   ['1','0','0','0','0','0','0','0','0','0','0','0','0'],
+//   ['1','1','1','1','1','1','1','1','1','1','1','1','E'],
+// ];
+
+describe('Maze business class can', () => {
+  test('return an instance of the Maze class', () => {
+    const maze = new Maze(SIMPLE_MAZE);
+    expect(maze.constructor.name).toEqual('Maze');
+  });
+
+  test('solve returns the solution', () => {
+    const maze = new Maze(SIMPLE_MAZE);
+    expect(maze.solve()).toEqual('you wish!');
+  });
+
+  // xtest('skip a test marked with "xtest" instead of "test"', () => {
+  //   expect(null).toEqual(null);
+  // });
+});
---
+++ packages/katas/kata-maze/src/maze.ts
@@ -0,0 +1,11 @@
+export class Maze {
+  maze: string[][];
+
+  constructor(maze: string[][]) {
+    this.maze = maze;
+  }
+  solve() {
+    const solution = 'you wish!';
+    return solution;
+  }
+}
