2ecd46c1a3691cf9c2fc018b92650db612a61f07
Michael van Vierssen <mvierssen@gmail.com>
2026-02-19 10:02:37 +0100

    feat: added initial kata-antenna

---
+++ packages/katas/kata-antenna/src/ports/antenna.ts
@@ -0,0 +1,4 @@
+export interface Antenna {
+  getMessage(): string;
+}
+
---
+++ packages/katas/kata-antenna/src/services/transformer.ts
@@ -0,0 +1,14 @@
+import type { Antenna } from "../ports/antenna.js";
+import type { Printer } from "../ports/printer.js";
+
+export class Transformer {
+  antenna: Antenna;
+  printer: Printer;
+  constructor(antenna: Antenna, printer: Printer) {
+    this.antenna = antenna;
+    this.printer = printer;
+  }
+
+  transform(): void {
+  }
+}
